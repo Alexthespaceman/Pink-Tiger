901b6f9d965c5dcde7a40fe7c95679627e2bb2aa
Alex Tristram <allex.tristram@gmail.com>
2023-03-03 00:15:57 +0000

    added loader and fetch method for nasa api

--- pink-tiger/src/js/Components/HomePage.js
+++ pink-tiger/src/js/Components/HomePage.js
@@ -1,10 +1,33 @@
-import changePage from "../functions/ChangePage";
 import React from 'react';
 
 class HomePage extends React.Component {
+    constructor(){
+        super()
+        this.state={ 
+            bool: true,
+            isLoaded: false,
+            content: {},
+        }
+    }
 
-    state={bool: true}
+    componentDidMount = () => {
+        fetch('https://api.nasa.gov/planetary/apod?api_key=moewalQmsvubYWWbTgIzQzSt9R8iaA3cjo0SJayL')
+          .then((res) => res.json())
+          .then((data) => {
+            if (data.ExceptionMessage) throw new Error(data.ExceptionMessage);
+            this.setState((prevState) => ({
+              isLoaded: !prevState.isLoaded,
+              content: data,
+            }));
+            console.log(this.state.content)
+          })
+          .catch((err) => {
+            console.error(`There was an error with the fetch request - (${err.message})`);
+            // window.location = '/error';
+        });
+    }
 
+    
     changeInitialPage(){
       const background = document.getElementById(this.props.props[1].homeScreenId);
       background.className = 'home_page-dark' ? background.className = 'home_page-light': null;
@@ -13,10 +36,20 @@ class HomePage extends React.Component {
       });
     }
 
+    nasaImg(){
+        return (
+            <div>
+                <p className='home_page-nasa-img-des'>{this.state.content.title}</p>
+                <img className='home_page-nasa-img' src={this.state.content.url}/>
+            </div>
+        )
+    }
+
     render(){
         return (
             <div id='home-screen' className='home_page-dark'>
-                { this.state.bool ? <div id='hello-message' className='home_page-hello-message' onClick={()=> this.changeInitialPage(this.state.bool)}> {this.props.props[0].title} </div> : null }
+               {this.state.isLoaded ? <div>loading</div> : this.state.bool ? <div id='hello-message' className='home_page-hello-message' onClick={()=> this.changeInitialPage(this.state.bool)}> {this.props.props[0].title} </div> : this.nasaImg()}
+                {/* { this.state.bool ? <div id='hello-message' className='home_page-hello-message' onClick={()=> this.changeInitialPage(this.state.bool)}> {this.props.props[0].title} </div> : this.nasaImg()} */}
             </div> 
         )
     }
