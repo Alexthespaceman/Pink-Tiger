433e75909a0a6251e4a9c116556a43f0a8400b3d
Alex Tristram <allex.tristram@gmail.com>
2023-03-11 11:29:49 +0000

    cleaned up the app

--- pink-tiger/src/js/App.js
+++ pink-tiger/src/js/App.js
@@ -1,12 +1,9 @@
 import '../index.css';
 import Header from './Components/Header';
 import PhotoDisplayer from './Components/PhotoDisplayer';
-// import SideBar from './Components/SideBar';
-
 function App() {
   return (
     <div className="App">
-      {/* <SideBar /> */}
       <Header />
       <PhotoDisplayer props={[{title:'Welcome, click here to see the wonders of the universe'}, {homeScreenId:'home-screen'}, {greetingMessageId:'hello-message'}]} />
     </div>
